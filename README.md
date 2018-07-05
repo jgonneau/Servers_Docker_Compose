79f0eae350e0d2790e3631640916d6e9cca0a9fa
jgonneau <jgonneau@j-p.work>
2018-07-06 07:21:39 +0900

    Commit 06/07

---
+++ README.md
@@ -0,0 +1,16 @@
+## How to start
+
+'''
+docker-compose up
+'''
+
+# Load balancing strategy
+
+'''
+least_conn
+'''
+
+# Name
+'''
+Gonneau Jean Paul
+'''
---
+++ site2/html/index.php
@@ -0,0 +1,14 @@
+<?php 
+	$variable = "Hyello!";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Site2</title>
+	<meta charset="utf-8" />
+</head>
+<body>
+	<h1><?= $variable ?></h1>
+	<p>test</p>
+</body>
+</html>
